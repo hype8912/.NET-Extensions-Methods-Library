7d2e81237775988f132ad2a0b70b93f7ab73bdb4
PatrickLorenz_cp <SND\PatrickLorenz_cp>
2009-08-24 14:52:10 +0000

    Added several Reflection helpers:
    - object.InvokeMethod<T>
    - object.GetPropertyValue<T>
    - object.SetPropertyValue
    - object.GetAttribute<T>
    - object.GetAttributes<T>
    - Type.CreateInstance
    
    Release as of dotnetpro 10/2009

--- PGK.Extensions/ObjectExtensions.cs
+++ PGK.Extensions/ObjectExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 
 /// <summary>
@@ -85,5 +86,193 @@ public static class ObjectExtensions {
             }
         }
         return false;
+    }
+
+    /// <summary>
+    /// Dynamically invokes a method using reflection
+    /// </summary>
+    /// <param name="obj">The object to perform on.</param>
+    /// <param name="methodName">The name of the method.</param>
+    /// <param name="parameters">The parameters passed to the method.</param>
+    /// <returns>The return value</returns>
+    /// <example>
+    /// <code>
+    /// var type = Type.GetType("System.IO.FileInfo, mscorlib");
+    /// var file = type.CreateInstance(@"c:\autoexec.bat");
+    /// if(file.GetPropertyValue<bool>("Exists")) {
+    ///  var reader = file.InvokeMethod<StreamReader>("OpenText");
+    ///  Console.WriteLine(reader.ReadToEnd());
+    ///  reader.Close();
+    /// }
+    /// </code>
+    /// </example>
+    public static object InvokeMethod(this object obj, string methodName, params object[] parameters) {
+        return InvokeMethod<object>(obj, methodName, parameters);
+    }
+
+    /// <summary>
+    /// Dynamically invokes a method using reflection and returns its value in a typed manner
+    /// </summary>
+    /// <typeparam name="T">The expected return data types</typeparam>
+    /// <param name="obj">The object to perform on.</param>
+    /// <param name="methodName">The name of the method.</param>
+    /// <param name="parameters">The parameters passed to the method.</param>
+    /// <returns>The return value</returns>
+    /// <example>
+    /// <code>
+    /// var type = Type.GetType("System.IO.FileInfo, mscorlib");
+    /// var file = type.CreateInstance(@"c:\autoexec.bat");
+    /// if(file.GetPropertyValue<bool>("Exists")) {
+    ///  var reader = file.InvokeMethod<StreamReader>("OpenText");
+    ///  Console.WriteLine(reader.ReadToEnd());
+    ///  reader.Close();
+    /// }
+    /// </code>
+    /// </example>
+    public static T InvokeMethod<T>(this object obj, string methodName, params object[] parameters) {
+        var type = obj.GetType();
+        var method = type.GetMethod(methodName);
+
+        if(method == null) throw new ArgumentException(string.Format("Method '{0}' not found.", methodName), methodName);
+
+        var value = method.Invoke(obj, parameters);
+        return (value is T ? (T) value : default(T));
+    }
+
+    /// <summary>
+    /// Dynamically retrieves a property value.
+    /// </summary>
+    /// <param name="obj">The object to perform on.</param>
+    /// <param name="propertyName">The Name of the property.</param>
+    /// <returns>The property value.</returns>
+    /// <example>
+    /// <code>
+    /// var type = Type.GetType("System.IO.FileInfo, mscorlib");
+    /// var file = type.CreateInstance(@"c:\autoexec.bat");
+    /// if(file.GetPropertyValue<bool>("Exists")) {
+    ///  var reader = file.InvokeMethod<StreamReader>("OpenText");
+    ///  Console.WriteLine(reader.ReadToEnd());
+    ///  reader.Close();
+    /// }
+    /// </code>
+    /// </example>
+    public static object GetPropertyValue(this object obj, string propertyName) {
+        return GetPropertyValue<object>(obj, propertyName, null);
+    }
+
+    /// <summary>
+    /// Dynamically retrieves a property value.
+    /// </summary>
+    /// <typeparam name="T">The expected return data type</typeparam>
+    /// <param name="obj">The object to perform on.</param>
+    /// <param name="propertyName">The Name of the property.</param>
+    /// <returns>The property value.</returns>
+    /// <example>
+    /// <code>
+    /// var type = Type.GetType("System.IO.FileInfo, mscorlib");
+    /// var file = type.CreateInstance(@"c:\autoexec.bat");
+    /// if(file.GetPropertyValue<bool>("Exists")) {
+    ///  var reader = file.InvokeMethod<StreamReader>("OpenText");
+    ///  Console.WriteLine(reader.ReadToEnd());
+    ///  reader.Close();
+    /// }
+    /// </code>
+    /// </example>
+    public static T GetPropertyValue<T>(this object obj, string propertyName) {
+        return GetPropertyValue<T>(obj, propertyName, default(T));
+    }
+
+    /// <summary>
+    /// Dynamically retrieves a property value.
+    /// </summary>
+    /// <typeparam name="T">The expected return data type</typeparam>
+    /// <param name="obj">The object to perform on.</param>
+    /// <param name="propertyName">The Name of the property.</param>
+    /// <param name="defaultValue">The default value to return.</param>
+    /// <returns>The property value.</returns>
+    /// <example>
+    /// <code>
+    /// var type = Type.GetType("System.IO.FileInfo, mscorlib");
+    /// var file = type.CreateInstance(@"c:\autoexec.bat");
+    /// if(file.GetPropertyValue<bool>("Exists")) {
+    ///  var reader = file.InvokeMethod<StreamReader>("OpenText");
+    ///  Console.WriteLine(reader.ReadToEnd());
+    ///  reader.Close();
+    /// }
+    /// </code>
+    /// </example>
+    public static T GetPropertyValue<T>(this object obj, string propertyName, T defaultValue) {
+        var type = obj.GetType();
+        var property = type.GetProperty(propertyName);
+
+        if(property == null) throw new ArgumentException(string.Format("Property '{0}' not found.", propertyName), propertyName);
+
+        var value = property.GetValue(obj, null);
+        return (value is T ? (T) value : defaultValue);
+    }
+
+    /// <summary>
+    /// Dynamically sets a property value.
+    /// </summary>
+    /// <param name="obj">The object to perform on.</param>
+    /// <param name="propertyName">The Name of the property.</param>
+    /// <param name="value">The value to be set.</param>
+    public static void SetPropertyValue(this object obj, string propertyName, object value) {
+        var type = obj.GetType();
+        var property = type.GetProperty(propertyName);
+
+        if(property == null) throw new ArgumentException(string.Format("Property '{0}' not found.", propertyName), propertyName);
+
+        property.SetValue(obj, value, null);
+    }
+
+    /// <summary>
+    /// Gets the first matching attribute defined on the data type.
+    /// </summary>
+    /// <typeparam name="T">The attribute type tp look for.</typeparam>
+    /// <param name="obj">The object to look on.</param>
+    /// <returns>The found attribute</returns>
+    public static T GetAttribute<T>(this object obj) where T : Attribute {
+        return GetAttribute<T>(obj, true);
+    }
+
+    /// <summary>
+    /// Gets the first matching attribute defined on the data type.
+    /// </summary>
+    /// <typeparam name="T">The attribute type tp look for.</typeparam>
+    /// <param name="obj">The object to look on.</param>
+    /// <param name="includeInherited">if set to <c>true</c> includes inherited attributes.</param>
+    /// <returns>The found attribute</returns>
+    public static T GetAttribute<T>(this object obj, bool includeInherited) where T : Attribute {
+        var type = (obj as Type ?? obj.GetType());
+        var attributes = type.GetCustomAttributes(typeof(T), includeInherited);
+        if((attributes != null) && (attributes.Length > 0)) {
+            return (attributes[0] as T);
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// Gets all matching attribute defined on the data type.
+    /// </summary>
+    /// <typeparam name="T">The attribute type tp look for.</typeparam>
+    /// <param name="obj">The object to look on.</param>
+    /// <returns>The found attributes</returns>
+    public static IEnumerable<T> GetAttributes<T>(this object obj) where T : Attribute {
+        return GetAttributes<T>(obj);
+    }
+
+    /// <summary>
+    /// Gets all matching attribute defined on the data type.
+    /// </summary>
+    /// <typeparam name="T">The attribute type tp look for.</typeparam>
+    /// <param name="obj">The object to look on.</param>
+    /// <param name="includeInherited">if set to <c>true</c> includes inherited attributes.</param>
+    /// <returns>The found attributes</returns>
+    public static IEnumerable<T> GetAttributes<T>(this object obj, bool includeInherited) where T : Attribute {
+        var type = (obj as Type ?? obj.GetType());
+        foreach(var attribute in type.GetCustomAttributes(typeof(T), includeInherited)) {
+            if(attribute is T) yield return (T) attribute;
+        }
     }
 }
