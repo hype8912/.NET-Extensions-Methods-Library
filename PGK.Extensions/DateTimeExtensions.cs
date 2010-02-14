4d61612cb551fe4f47e653fb26f8b473bf556521
PatrickLorenz_cp <SND\PatrickLorenz_cp>
2010-02-14 11:01:20 +0000

    Added extension methods for DateTime and DateTimeOffset providing a lot of overloads to specify CultureInfo and TimeZoneInfo to be used for any conversions:
    - DateTime.SetTime
    - DateTime.ToDateTimeOffset
    - DateTime.GetFirstDayOfWeek
    - DateTime.GetLastDayOfWeek
    - DateTime.GetWeeksWeekday
    - DateTime.GetNextWeekday
    - DateTime.GetPreviousWeekday
    
    - DateTimeOffset.SetTime
    - DateTimeOffset.ToLocalDateTime
    
    Release as of dotnetpro 04/2010

--- PGK.Extensions/DateTimeExtensions.cs
+++ PGK.Extensions/DateTimeExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 /// <summary>
 /// Extension methods for the DateTimeOffset data type.
@@ -91,6 +92,154 @@ public static class DateTimeExtensions {
     /// </returns>
     public static bool IsToday(this DateTime dt) {
         return (dt.Date == DateTime.Today);
+    }
+
+    /// <summary>
+    /// Sets the time on the specified DateTime value.
+    /// </summary>
+    /// <param name="date">The base date.</param>
+    /// <param name="hours">The hours to be set.</param>
+    /// <param name="minutes">The minutes to be set.</param>
+    /// <param name="seconds">The seconds to be set.</param>
+    /// <returns>The DateTime including the new time value</returns>
+    public static DateTime SetTime(this DateTime date, int hours, int minutes, int seconds) {
+        return date.SetTime(new TimeSpan(hours, minutes, seconds));
+    }
+
+    /// <summary>
+    /// Sets the time on the specified DateTime value.
+    /// </summary>
+    /// <param name="date">The base date.</param>
+    /// <param name="time">The TimeSpan to be applied.</param>
+    /// <returns>
+    /// The DateTime including the new time value
+    /// </returns>
+    public static DateTime SetTime(this DateTime date, TimeSpan time) {
+        return date.Date.Add(time);
+    }
+
+    /// <summary>
+    /// Converts a DateTime into a DateTimeOffset using the local system time zone.
+    /// </summary>
+    /// <param name="localDateTime">The local DateTime.</param>
+    /// <returns>The converted DateTimeOffset</returns>
+    public static DateTimeOffset ToDateTimeOffset(this DateTime localDateTime) {
+        return localDateTime.ToDateTimeOffset(null);
+    }
+
+    /// <summary>
+    /// Converts a DateTime into a DateTimeOffset using the specified time zone.
+    /// </summary>
+    /// <param name="localDateTime">The local DateTime.</param>
+    /// <param name="localTimeZone">The local time zone.</param>
+    /// <returns>The converted DateTimeOffset</returns>
+    public static DateTimeOffset ToDateTimeOffset(this DateTime localDateTime, TimeZoneInfo localTimeZone) {
+        localTimeZone = (localTimeZone ?? TimeZoneInfo.Local);
+
+        if(localDateTime.Kind != DateTimeKind.Unspecified) {
+            localDateTime = new DateTime(localDateTime.Ticks, DateTimeKind.Unspecified);
+        }
+     
+        return TimeZoneInfo.ConvertTimeToUtc(localDateTime, localTimeZone);
+    }
+
+    /// <summary>
+    /// Gets the first day of the week using the current culture.
+    /// </summary>
+    /// <param name="date">The date.</param>
+    /// <returns>The first day of the week</returns>
+    public static DateTime GetFirstDayOfWeek(this DateTime date) {
+        return date.GetFirstDayOfWeek(null);
+    }
+
+    /// <summary>
+    /// Gets the first day of the week using the specified culture.
+    /// </summary>
+    /// <param name="date">The date.</param>
+    /// <param name="cultureInfo">The culture to determine the first weekday of a week.</param>
+    /// <returns>The first day of the week</returns>
+    public static DateTime GetFirstDayOfWeek(this DateTime date, CultureInfo cultureInfo) {
+        cultureInfo = (cultureInfo ?? CultureInfo.CurrentCulture);
+
+        var firstDayOfWeek = cultureInfo.DateTimeFormat.FirstDayOfWeek;
+        while(date.DayOfWeek != firstDayOfWeek) date = date.AddDays(-1);
+
+        return date;
+    }
+
+    /// <summary>
+    /// Gets the last day of the week using the current culture.
+    /// </summary>
+    /// <param name="date">The date.</param>
+    /// <returns>The first day of the week</returns>
+    public static DateTime GetLastDayOfWeek(this DateTime date) {
+        return date.GetLastDayOfWeek(null);
+    }
+
+    /// <summary>
+    /// Gets the last day of the week using the specified culture.
+    /// </summary>
+    /// <param name="date">The date.</param>
+    /// <param name="cultureInfo">The culture to determine the first weekday of a week.</param>
+    /// <returns>The first day of the week</returns>
+    public static DateTime GetLastDayOfWeek(this DateTime date, CultureInfo cultureInfo) {
+        return date.GetFirstDayOfWeek(cultureInfo).AddDays(6);
+    }
+
+    /// <summary>
+    /// Gets the next occurence of the specified weekday within the current week using the current culture.
+    /// </summary>
+    /// <param name="date">The base date.</param>
+    /// <param name="weekday">The desired weekday.</param>
+    /// <returns>The calculated date.</returns>
+    /// <example><code>
+    /// var thisWeeksMonday = DateTime.Now.GetWeekday(DayOfWeek.Monday);
+    /// </code></example>
+    public static DateTime GetWeeksWeekday(this DateTime date, DayOfWeek weekday) {
+        return date.GetWeeksWeekday(weekday, null);
+    }
+
+    /// <summary>
+    /// Gets the next occurence of the specified weekday within the current week using the specified culture.
+    /// </summary>
+    /// <param name="date">The base date.</param>
+    /// <param name="weekday">The desired weekday.</param>
+    /// <param name="cultureInfo">The culture to determine the first weekday of a week.</param>
+    /// <returns>The calculated date.</returns>
+    /// <example><code>
+    /// var thisWeeksMonday = DateTime.Now.GetWeekday(DayOfWeek.Monday);
+    /// </code></example>
+    public static DateTime GetWeeksWeekday(this DateTime date, DayOfWeek weekday, CultureInfo cultureInfo) {
+        var firstDayOfWeek = date.GetFirstDayOfWeek(cultureInfo);
+        return firstDayOfWeek.GetNextWeekday(weekday);
+    }
+
+    /// <summary>
+    /// Gets the next occurence of the specified weekday.
+    /// </summary>
+    /// <param name="date">The base date.</param>
+    /// <param name="weekday">The desired weekday.</param>
+    /// <returns>The calculated date.</returns>
+    /// <example><code>
+    /// var lastMonday = DateTime.Now.GetNextWeekday(DayOfWeek.Monday);
+    /// </code></example>
+    public static DateTime GetNextWeekday(this DateTime date, DayOfWeek weekday) {
+        while(date.DayOfWeek != weekday) date = date.AddDays(1);
+        return date;
+    }
+
+    /// <summary>
+    /// Gets the previous occurence of the specified weekday.
+    /// </summary>
+    /// <param name="date">The base date.</param>
+    /// <param name="weekday">The desired weekday.</param>
+    /// <returns>The calculated date.</returns>
+    /// <example><code>
+    /// var lastMonday = DateTime.Now.GetPreviousWeekday(DayOfWeek.Monday);
+    /// </code></example>
+    public static DateTime GetPreviousWeekday(this DateTime date, DayOfWeek weekday) {
+        while(date.DayOfWeek != weekday) date = date.AddDays(-1);
+        return date;
     }
 }
 
