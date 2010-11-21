182c3908973ca08ead1f5711ba072763e9b709f2
sx_cp <SND\sx_cp>
2010-11-21 10:26:10 +0000

    Added new extensions for int: - int.IsEven - int.IsOdd
    Added new tests for IntExtensions
     - test IsEven
     - test IsOdd

---
+++ PGK.Extensions.Tests/IntExtensionsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PGK.Extensions.Tests
+{
+    [TestClass]
+    public class IntExtensionsTest
+    {
+        [TestMethod]
+        public void TestIsEven()
+        {
+            /* positive testing */
+            int testValue = 4;
+            Assert.IsTrue(testValue.IsEven());
+            /* negative testing */
+            testValue = 3;
+            Assert.IsFalse(testValue.IsEven());
+        }
+
+        [TestMethod]
+        public void TestIsOdd()
+        {
+            /* positive testing */
+            int testValue = 3;
+            Assert.IsTrue(testValue.IsOdd());
+            /* negative testing */
+            testValue = 4;
+            Assert.IsFalse(testValue.IsOdd());
+        }
+
+    }
+}
--- PGK.Extensions/IntExtensions.cs
+++ PGK.Extensions/IntExtensions.cs
@@ -21,4 +21,24 @@ public static class IntExtensions {
     public static void Times(this int value, Action<int> action) {
         for (var i = 0; i < value; i++) action(i);
     }
+
+    /// <summary>
+    /// Determines whether the value is even
+    /// </summary>
+    /// <param name="value">The Value</param>
+    /// <returns>true or false</returns>
+    public static bool IsEven(this int value) 
+    {
+        return value % 2 == 0;
+    }
+
+    /// <summary>
+    /// Determines whether the value is odd
+    /// </summary>
+    /// <param name="value">The Value</param>
+    /// <returns>true or false</returns>
+    public static bool IsOdd(this int value)
+    {
+        return !IsEven(value);
+    }
 }
