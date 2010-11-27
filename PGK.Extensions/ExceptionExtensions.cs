68c94cf31720173a3120bdbfe20c369e2c4a8c25
PatrickLorenz_cp <SND\PatrickLorenz_cp>
2010-11-27 15:03:21 +0000

    Updated project settings (and VS added some unchanged files as well?!)

---
+++ PGK.Extensions/ExceptionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+///   Extension methods for all kinds of exceptions.
+/// </summary>
+public static class ExceptionExtensions {
+    /// <summary>
+    ///   Gets the original exception which is most inner exception.
+    /// </summary>
+    /// <param name = "exception">The exeption</param>
+    /// <returns>The original exception</returns>
+    /// <remarks>
+    ///   Contributed by Kenneth Scott
+    /// </remarks>
+    public static Exception GetOriginalException(this Exception exception) {
+        if (exception.InnerException == null) return exception;
+
+        return exception.InnerException.GetOriginalException();
+    }
+}
