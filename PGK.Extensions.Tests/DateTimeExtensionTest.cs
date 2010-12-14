f18ca7dc43a4e34ea42b83a4c0a38dce38d5e03e
kcanasa_cp <SND\kcanasa_cp>
2010-12-14 01:08:04 +0000

    Added + TestCalculateAge + TestGetDaysInYear

--- PGK.Extensions.Tests/DateTimeExtensionTest.cs
+++ PGK.Extensions.Tests/DateTimeExtensionTest.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should.Fluent;
 
 namespace PGK.Extensions.Tests
 {
@@ -20,5 +21,46 @@ namespace PGK.Extensions.Tests
 			testValue = DateTime.Now.AddYears(1);
 			Assert.AreNotEqual(testValue.GetDays(), resultValue);
 		}
+
+        [TestMethod]
+        public void TestCalculateAge()
+        {
+            // Arrange
+            var value = new DateTime(1984, 12, 26);
+            var futureDate = new DateTime(2011, 01, 01);
+            var defaultValue = 25;
+            var smallValue = 24;
+            var bigValue = 26;
+            
+            // Act
+            var result = value.CalculateAge();
+            var futureResult = value.CalculateAge(futureDate);
+
+            // Assert
+            result.Should().Equal(defaultValue);
+            result.Should().Not.Equal(smallValue);
+            result.Should().Not.Equal(bigValue);
+
+            futureResult.Should().Equal(bigValue);
+            futureResult.Should().Not.Equal(smallValue);
+            futureResult.Should().Not.Equal(defaultValue);
+        }
+
+        [TestMethod]
+        public void TestGetCountDaysOfMonth()
+        {
+            // Arrange
+            var value = new DateTime(2010, 1, 1);
+            var defaultValue = 31;
+
+            // Act
+            var result = value.GetCountDaysOfMonth();
+
+            // Assert
+            result.Should().Equal(defaultValue);
+            result.Should().Not.Equal(0);
+        }
+
+
 	}
 }
