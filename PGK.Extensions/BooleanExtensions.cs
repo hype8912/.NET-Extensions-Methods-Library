364e9c122d4f06f766e519b0665d428f8302cd7f
akronite_cp <SND\akronite_cp>
2011-03-21 01:25:15 +0000

    added boolean.ToYesNoString

---
+++ PGK.Extensions/BooleanExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class BooleanExtensions
+{
+	/// <summary>
+	/// Converts the value of this instance to its equivalent string representation (either "Yes" or "No").
+	/// </summary>
+	/// <param name="boolean"></param>
+	/// <returns>string</returns>
+	public static string ToYesNoString(this Boolean boolean)
+	{
+		return boolean ? "Yes" : "No";
+	}
+}
