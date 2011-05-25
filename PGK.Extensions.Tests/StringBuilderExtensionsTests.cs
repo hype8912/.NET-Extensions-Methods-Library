2a486b2895bdb5f0df8f63d36658b68fed7270b8
jesuissur_cp <SND\jesuissur_cp>
2011-05-25 16:15:33 +0000

    StringBuilderExtensions added (AppendLine with support for string format)

---
+++ PGK.Extensions.Tests/StringBuilderExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should.Fluent;
+
+namespace PGK.Extensions.Tests
+{
+	[TestClass]
+    public class StringBuilderExtensionsTests
+	{
+		[TestMethod]
+		public void AppendLineWithFormat()
+		{
+		    var builder = new StringBuilder();
+		    string result;
+
+            // Arrange
+		    builder.AppendLine("Something to format ({0}.{1}) with a new line after", "String", "Format");
+		    // Act
+            result = builder.ToString();
+		    // Assert
+            result.Should().Not.Be.NullOrEmpty();
+		    result.Split(Environment.NewLine).Should().Count.Exactly(2);
+            result.Split(Environment.NewLine)[0].Should().Equal("Something to format (String.Format) with a new line after");
+            result.Split(Environment.NewLine)[1].Should().Be.Empty();
+		}
+	}
+}
