08929f0d65210caa88bab8e55cb619ad2cb38aae
thinktech_coder_cp <SND\thinktech_coder_cp>
2011-08-02 02:41:01 +0000

    Select overload for Enumerable Extension.

--- PGK.Extensions/EnumerableExtensions.cs
+++ PGK.Extensions/EnumerableExtensions.cs
@@ -8,263 +8,288 @@ using System.Text;
 /// </summary>
 public static class EnumerableExtensions
 {
-	/// <summary>
-	/// 	Converts all items of a list and returns them as enumerable.
-	/// </summary>
-	/// <typeparam name = "TSource">The source data type</typeparam>
-	/// <typeparam name = "TTarget">The target data type</typeparam>
-	/// <param name = "source">The source data.</param>
-	/// <returns>The converted data</returns>
-	/// <example>
-	/// 	var values = new[] { "1", "2", "3" };
-	/// 	values.ConvertList&lt;string, int&gt;().ForEach(Console.WriteLine);
-	/// </example>
-	public static IEnumerable<TTarget> ConvertList<TSource, TTarget>(this IEnumerable<TSource> source)
-	{
-		return source.Select(value => value.ConvertTo<TTarget>());
-	}
+    /// <summary>
+    /// 	Converts all items of a list and returns them as enumerable.
+    /// </summary>
+    /// <typeparam name = "TSource">The source data type</typeparam>
+    /// <typeparam name = "TTarget">The target data type</typeparam>
+    /// <param name = "source">The source data.</param>
+    /// <returns>The converted data</returns>
+    /// <example>
+    /// 	var values = new[] { "1", "2", "3" };
+    /// 	values.ConvertList&lt;string, int&gt;().ForEach(Console.WriteLine);
+    /// </example>
+    public static IEnumerable<TTarget> ConvertList<TSource, TTarget>(this IEnumerable<TSource> source)
+    {
+        return source.Select(value => value.ConvertTo<TTarget>());
+    }
 
-	/// <summary>
-	/// 	Performs an action for each item in the enumerable
-	/// </summary>
-	/// <typeparam name = "T">The enumerable data type</typeparam>
-	/// <param name = "values">The data values.</param>
-	/// <param name = "action">The action to be performed.</param>
-	/// <example>
-	/// 	var values = new[] { "1", "2", "3" };
-	/// 	values.ConvertList&lt;string, int&gt;().ForEach(Console.WriteLine);
-	/// </example>
-	/// <remarks>
-	/// 	This method was intended to return the passed values to provide method chaining. Howver due to defered execution the compiler would actually never run the entire code at all.
-	/// </remarks>
-	public static void ForEach<T>(this IEnumerable<T> values, Action<T> action)
-	{
-		foreach (var value in values)
-			action(value);
-	}
+    /// <summary>
+    /// 	Performs an action for each item in the enumerable
+    /// </summary>
+    /// <typeparam name = "T">The enumerable data type</typeparam>
+    /// <param name = "values">The data values.</param>
+    /// <param name = "action">The action to be performed.</param>
+    /// <example>
+    /// 	var values = new[] { "1", "2", "3" };
+    /// 	values.ConvertList&lt;string, int&gt;().ForEach(Console.WriteLine);
+    /// </example>
+    /// <remarks>
+    /// 	This method was intended to return the passed values to provide method chaining. Howver due to defered execution the compiler would actually never run the entire code at all.
+    /// </remarks>
+    public static void ForEach<T>(this IEnumerable<T> values, Action<T> action)
+    {
+        foreach (var value in values)
+            action(value);
+    }
 
-	///<summary>
-	///	Returns enumerable object based on target, which does not contains null references.
-	///	If target is null reference, returns empty enumerable object.
-	///</summary>
-	///<typeparam name = "T">Type of items in target.</typeparam>
-	///<param name = "target">Target enumerable object. Can be null.</param>
-	///<example>
-	///	object[] items = null;
-	///	foreach(var item in items.NotNull()){
-	///	// result of items.NotNull() is empty but not null enumerable
-	///	}
-	/// 
-	///	object[] items = new object[]{ null, "Hello World!", null, "Good bye!" };
-	///	foreach(var item in items.NotNull()){
-	///	// result of items.NotNull() is enumerable with two strings
-	///	}
-	///</example>
-	///<remarks>
-	///	Contributed by tencokacistromy, http://www.codeplex.com/site/users/view/tencokacistromy
-	///</remarks>
-	public static IEnumerable<T> IgnoreNulls<T>(this IEnumerable<T> target)
-	{
-		if (ReferenceEquals(target, null))
-			yield break;
+    ///<summary>
+    ///	Returns enumerable object based on target, which does not contains null references.
+    ///	If target is null reference, returns empty enumerable object.
+    ///</summary>
+    ///<typeparam name = "T">Type of items in target.</typeparam>
+    ///<param name = "target">Target enumerable object. Can be null.</param>
+    ///<example>
+    ///	object[] items = null;
+    ///	foreach(var item in items.NotNull()){
+    ///	// result of items.NotNull() is empty but not null enumerable
+    ///	}
+    /// 
+    ///	object[] items = new object[]{ null, "Hello World!", null, "Good bye!" };
+    ///	foreach(var item in items.NotNull()){
+    ///	// result of items.NotNull() is enumerable with two strings
+    ///	}
+    ///</example>
+    ///<remarks>
+    ///	Contributed by tencokacistromy, http://www.codeplex.com/site/users/view/tencokacistromy
+    ///</remarks>
+    public static IEnumerable<T> IgnoreNulls<T>(this IEnumerable<T> target)
+    {
+        if (ReferenceEquals(target, null))
+            yield break;
 
-		foreach (var item in target.Where(item => !ReferenceEquals(item, null)))
-			yield return item;
-	}
+        foreach (var item in target.Where(item => !ReferenceEquals(item, null)))
+            yield return item;
+    }
 
-	/// <summary>
-	/// 	Returns the maximum item based on a provided selector.
-	/// </summary>
-	/// <typeparam name = "TItem">The item type</typeparam>
-	/// <typeparam name = "TValue">The value item</typeparam>
-	/// <param name = "items">The items.</param>
-	/// <param name = "selector">The selector.</param>
-	/// <param name = "maxValue">The max value as output parameter.</param>
-	/// <returns>The maximum item</returns>
-	/// <example>
-	/// 	<code>
-	/// 		int age;
-	/// 		var oldestPerson = persons.MaxItem(p =&gt; p.Age, out age);
-	/// 	</code>
-	/// </example>
-	public static TItem MaxItem<TItem, TValue>(this IEnumerable<TItem> items, Func<TItem, TValue> selector, out TValue maxValue) where TItem : class where TValue : IComparable
-	{
-		TItem maxItem = null;
-		maxValue = default(TValue);
+    /// <summary>
+    /// 	Returns the maximum item based on a provided selector.
+    /// </summary>
+    /// <typeparam name = "TItem">The item type</typeparam>
+    /// <typeparam name = "TValue">The value item</typeparam>
+    /// <param name = "items">The items.</param>
+    /// <param name = "selector">The selector.</param>
+    /// <param name = "maxValue">The max value as output parameter.</param>
+    /// <returns>The maximum item</returns>
+    /// <example>
+    /// 	<code>
+    /// 		int age;
+    /// 		var oldestPerson = persons.MaxItem(p =&gt; p.Age, out age);
+    /// 	</code>
+    /// </example>
+    public static TItem MaxItem<TItem, TValue>(this IEnumerable<TItem> items, Func<TItem, TValue> selector, out TValue maxValue) where TItem : class where TValue : IComparable
+    {
+        TItem maxItem = null;
+        maxValue = default(TValue);
 
-		foreach (var item in items)
-		{
-			if (item == null)
-				continue;
+        foreach (var item in items)
+        {
+            if (item == null)
+                continue;
 
-			var itemValue = selector(item);
+            var itemValue = selector(item);
 
-			if ((maxItem != null) && (itemValue.CompareTo(maxValue) <= 0))
-				continue;
+            if ((maxItem != null) && (itemValue.CompareTo(maxValue) <= 0))
+                continue;
 
-			maxValue = itemValue;
-			maxItem = item;
-		}
+            maxValue = itemValue;
+            maxItem = item;
+        }
 
-		return maxItem;
-	}
+        return maxItem;
+    }
 
-	/// <summary>
-	/// 	Returns the maximum item based on a provided selector.
-	/// </summary>
-	/// <typeparam name = "TItem">The item type</typeparam>
-	/// <typeparam name = "TValue">The value item</typeparam>
-	/// <param name = "items">The items.</param>
-	/// <param name = "selector">The selector.</param>
-	/// <returns>The maximum item</returns>
-	/// <example>
-	/// 	<code>
-	/// 		var oldestPerson = persons.MaxItem(p =&gt; p.Age);
-	/// 	</code>
-	/// </example>
-	public static TItem MaxItem<TItem, TValue>(this IEnumerable<TItem> items, Func<TItem, TValue> selector) where TItem : class where TValue : IComparable
-	{
-		TValue maxValue;
+    /// <summary>
+    /// 	Returns the maximum item based on a provided selector.
+    /// </summary>
+    /// <typeparam name = "TItem">The item type</typeparam>
+    /// <typeparam name = "TValue">The value item</typeparam>
+    /// <param name = "items">The items.</param>
+    /// <param name = "selector">The selector.</param>
+    /// <returns>The maximum item</returns>
+    /// <example>
+    /// 	<code>
+    /// 		var oldestPerson = persons.MaxItem(p =&gt; p.Age);
+    /// 	</code>
+    /// </example>
+    public static TItem MaxItem<TItem, TValue>(this IEnumerable<TItem> items, Func<TItem, TValue> selector) where TItem : class where TValue : IComparable
+    {
+        TValue maxValue;
 
-		return items.MaxItem(selector, out maxValue);
-	}
+        return items.MaxItem(selector, out maxValue);
+    }
 
-	/// <summary>
-	/// 	Returns the minimum item based on a provided selector.
-	/// </summary>
-	/// <typeparam name = "TItem">The item type</typeparam>
-	/// <typeparam name = "TValue">The value item</typeparam>
-	/// <param name = "items">The items.</param>
-	/// <param name = "selector">The selector.</param>
-	/// <param name = "minValue">The min value as output parameter.</param>
-	/// <returns>The minimum item</returns>
-	/// <example>
-	/// 	<code>
-	/// 		int age;
-	/// 		var youngestPerson = persons.MinItem(p =&gt; p.Age, out age);
-	/// 	</code>
-	/// </example>
-	public static TItem MinItem<TItem, TValue>(this IEnumerable<TItem> items, Func<TItem, TValue> selector, out TValue minValue) where TItem : class where TValue : IComparable
-	{
-		TItem minItem = null;
-		minValue = default(TValue);
+    /// <summary>
+    /// 	Returns the minimum item based on a provided selector.
+    /// </summary>
+    /// <typeparam name = "TItem">The item type</typeparam>
+    /// <typeparam name = "TValue">The value item</typeparam>
+    /// <param name = "items">The items.</param>
+    /// <param name = "selector">The selector.</param>
+    /// <param name = "minValue">The min value as output parameter.</param>
+    /// <returns>The minimum item</returns>
+    /// <example>
+    /// 	<code>
+    /// 		int age;
+    /// 		var youngestPerson = persons.MinItem(p =&gt; p.Age, out age);
+    /// 	</code>
+    /// </example>
+    public static TItem MinItem<TItem, TValue>(this IEnumerable<TItem> items, Func<TItem, TValue> selector, out TValue minValue) where TItem : class where TValue : IComparable
+    {
+        TItem minItem = null;
+        minValue = default(TValue);
 
-		foreach (var item in items)
-		{
-			if (item == null)
-				continue;
-			var itemValue = selector(item);
+        foreach (var item in items)
+        {
+            if (item == null)
+                continue;
+            var itemValue = selector(item);
 
-			if ((minItem != null) && (itemValue.CompareTo(minValue) >= 0))
-				continue;
-			minValue = itemValue;
-			minItem = item;
-		}
+            if ((minItem != null) && (itemValue.CompareTo(minValue) >= 0))
+                continue;
+            minValue = itemValue;
+            minItem = item;
+        }
 
-		return minItem;
-	}
+        return minItem;
+    }
 
-	/// <summary>
-	/// 	Returns the minimum item based on a provided selector.
-	/// </summary>
-	/// <typeparam name = "TItem">The item type</typeparam>
-	/// <typeparam name = "TValue">The value item</typeparam>
-	/// <param name = "items">The items.</param>
-	/// <param name = "selector">The selector.</param>
-	/// <returns>The minimum item</returns>
-	/// <example>
-	/// 	<code>
-	/// 		var youngestPerson = persons.MinItem(p =&gt; p.Age);
-	/// 	</code>
-	/// </example>
-	public static TItem MinItem<TItem, TValue>(this IEnumerable<TItem> items, Func<TItem, TValue> selector) where TItem : class where TValue : IComparable
-	{
-		TValue minValue;
+    /// <summary>
+    /// 	Returns the minimum item based on a provided selector.
+    /// </summary>
+    /// <typeparam name = "TItem">The item type</typeparam>
+    /// <typeparam name = "TValue">The value item</typeparam>
+    /// <param name = "items">The items.</param>
+    /// <param name = "selector">The selector.</param>
+    /// <returns>The minimum item</returns>
+    /// <example>
+    /// 	<code>
+    /// 		var youngestPerson = persons.MinItem(p =&gt; p.Age);
+    /// 	</code>
+    /// </example>
+    public static TItem MinItem<TItem, TValue>(this IEnumerable<TItem> items, Func<TItem, TValue> selector) where TItem : class where TValue : IComparable
+    {
+        TValue minValue;
 
-		return items.MinItem(selector, out minValue);
-	}
+        return items.MinItem(selector, out minValue);
+    }
 
-	///<summary>
-	///	Get Distinct
-	///</summary>
-	///<param name = "source"></param>
-	///<param name = "expression"></param>
-	///<typeparam name = "T"></typeparam>
-	///<typeparam name = "TKey"></typeparam>
-	///<returns></returns>
-	/// <remarks>
-	/// 	Contributed by Michael T, http://about.me/MichaelTran
-	/// </remarks>
-	public static IEnumerable<T> Distinct<T, TKey>(this IEnumerable<T> source, Func<T, TKey> expression)
-	{
-		return source == null ? Enumerable.Empty<T>() : source.GroupBy(expression).Select(i => i.First());
-	}
+    ///<summary>
+    ///	Get Distinct
+    ///</summary>
+    ///<param name = "source"></param>
+    ///<param name = "expression"></param>
+    ///<typeparam name = "T"></typeparam>
+    ///<typeparam name = "TKey"></typeparam>
+    ///<returns></returns>
+    /// <remarks>
+    /// 	Contributed by Michael T, http://about.me/MichaelTran
+    /// </remarks>
+    public static IEnumerable<T> Distinct<T, TKey>(this IEnumerable<T> source, Func<T, TKey> expression)
+    {
+        return source == null ? Enumerable.Empty<T>() : source.GroupBy(expression).Select(i => i.First());
+    }
 
-	///<summary>
-	///	Remove item from a list
-	///</summary>
-	///<param name = "source"></param>
-	///<param name = "predicate"></param>
-	///<typeparam name = "T"></typeparam>
-	///<returns></returns>
-	/// <remarks>
-	/// 	Contributed by Michael T, http://about.me/MichaelTran
-	/// </remarks>
-	public static IEnumerable<T> RemoveAll<T>(this IEnumerable<T> source, Predicate<T> predicate)
-	{
-		if (source == null)
-			return Enumerable.Empty<T>();
+    ///<summary>
+    ///	Remove item from a list
+    ///</summary>
+    ///<param name = "source"></param>
+    ///<param name = "predicate"></param>
+    ///<typeparam name = "T"></typeparam>
+    ///<returns></returns>
+    /// <remarks>
+    /// 	Contributed by Michael T, http://about.me/MichaelTran
+    /// </remarks>
+    public static IEnumerable<T> RemoveAll<T>(this IEnumerable<T> source, Predicate<T> predicate)
+    {
+        if (source == null)
+            return Enumerable.Empty<T>();
 
-		var list = source.ToList();
-		list.RemoveAll(predicate);
-		return list;
-	}
+        var list = source.ToList();
+        list.RemoveAll(predicate);
+        return list;
+    }
 
-	///<summary>
-	/// Turn the list of objects to a string of Common Seperated Value
-	///</summary>
-	///<param name="source"></param>
-	///<param name="separator"></param>
-	///<typeparam name="T"></typeparam>
-	///<returns></returns>
-	/// <example>
-	/// 	<code>
-	/// 		var values = new[] { 1, 2, 3, 4, 5 };
-	///			string csv = values.ToCSV(';');
-	/// 	</code>
-	/// </example>
-	/// <remarks>
-	/// 	Contributed by Moses, http://mosesofegypt.net
-	/// </remarks>
-	public static string ToCSV<T>(this IEnumerable<T> source, char separator)
-	{
-		if (source == null)
-			return string.Empty;
+    ///<summary>
+    /// Turn the list of objects to a string of Common Seperated Value
+    ///</summary>
+    ///<param name="source"></param>
+    ///<param name="separator"></param>
+    ///<typeparam name="T"></typeparam>
+    ///<returns></returns>
+    /// <example>
+    /// 	<code>
+    /// 		var values = new[] { 1, 2, 3, 4, 5 };
+    ///			string csv = values.ToCSV(';');
+    /// 	</code>
+    /// </example>
+    /// <remarks>
+    /// 	Contributed by Moses, http://mosesofegypt.net
+    /// </remarks>
+    public static string ToCSV<T>(this IEnumerable<T> source, char separator)
+    {
+        if (source == null)
+            return string.Empty;
 
-		var csv = new StringBuilder();
-		source.ForEach(value => csv.AppendFormat("{0}{1}", value, separator));
-		return csv.ToString(0, csv.Length - 1);
-	}
+        var csv = new StringBuilder();
+        source.ForEach(value => csv.AppendFormat("{0}{1}", value, separator));
+        return csv.ToString(0, csv.Length - 1);
+    }
 
-	///<summary>
-	/// Turn the list of objects to a string of Common Seperated Value
-	///</summary>
-	///<param name="source"></param>
-	///<typeparam name="T"></typeparam>
-	///<returns></returns>
-	/// <example>
-	/// 	<code>
-	/// 		var values = new[] {1, 2, 3, 4, 5};
-	///			string csv = values.ToCSV();
-	/// 	</code>
-	/// </example>
-	/// <remarks>
-	/// 	Contributed by Moses, http://mosesofegypt.net
-	/// </remarks>
-	public static string ToCSV<T>(this IEnumerable<T> source)
-	{
-		return source == null ? string.Empty : source.ToCSV(',');
-	}
+    ///<summary>
+    /// Turn the list of objects to a string of Common Seperated Value
+    ///</summary>
+    ///<param name="source"></param>
+    ///<typeparam name="T"></typeparam>
+    ///<returns></returns>
+    /// <example>
+    /// 	<code>
+    /// 		var values = new[] {1, 2, 3, 4, 5};
+    ///			string csv = values.ToCSV();
+    /// 	</code>
+    /// </example>
+    /// <remarks>
+    /// 	Contributed by Moses, http://mosesofegypt.net
+    /// </remarks>
+    public static string ToCSV<T>(this IEnumerable<T> source)
+    {
+        return source == null ? string.Empty : source.ToCSV(',');
+    }
+
+    /// <summary>
+    /// Overload the Select to allow null as a return
+    /// </summary>
+    /// <typeparam name="TSource"></typeparam>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="source"></param>
+    /// <param name="selector"></param>
+    /// <param name="allowNull"></param>
+    /// <returns>An <see cref="IEnumerable{TResult}"/> using the selector containing null or non-null results based on <see cref="allowNull"/>.</returns>
+    /// <example>
+    /// <code>
+    /// var list = new List{object}{ new object(), null, null };
+    /// var noNulls = list.Select(x => x, false);
+    /// </code>
+    /// </example>
+    public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector, bool allowNull = true)
+    {
+        foreach (var item in source)
+        {
+            var select = selector(item);
+            if (allowNull || !Equals(select, default(TSource)))
+                yield return select;
+        }
+    }
 
     /// <summary>
     /// Returns true if the <paramref name="source"/> is null or without any items.
