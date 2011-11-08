81b4e7f87ec9e65e29b5914e687fc302f9ed961c
akronite_cp <SND\akronite_cp>
2011-11-08 00:20:12 +0000

    Added .NET 4.0 Extension project (PGK.Extensions4 and PGK.Test4) // 4 because .net 4 + StringExtensions.ToPlural2() (to resolves issue #6589) + StringExtensionsTest

---
+++ PGK.Extensions.Test4/StringExtensionsTest.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Globalization;
+
+namespace PGK.Extensions.Test4
+{
+
+
+	/// <summary>
+	///This is a test class for StringExtensionsTest and is intended
+	///to contain all StringExtensionsTest Unit Tests
+	///</summary>
+	[TestClass()]
+	public class StringExtensionsTest
+	{
+
+
+		private TestContext testContextInstance;
+
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext
+		{
+			get
+			{
+				return testContextInstance;
+			}
+			set
+			{
+				testContextInstance = value;
+			}
+		}
+
+		#region Additional test attributes
+		// 
+		//You can use the following additional attributes as you write your tests:
+		//
+		//Use ClassInitialize to run code before running the first test in the class
+		//[ClassInitialize()]
+		//public static void MyClassInitialize(TestContext testContext)
+		//{
+		//}
+		//
+		//Use ClassCleanup to run code after all tests in a class have run
+		//[ClassCleanup()]
+		//public static void MyClassCleanup()
+		//{
+		//}
+		//
+		//Use TestInitialize to run code before running each test
+		//[TestInitialize()]
+		//public void MyTestInitialize()
+		//{
+		//}
+		//
+		//Use TestCleanup to run code after each test has run
+		//[TestCleanup()]
+		//public void MyTestCleanup()
+		//{
+		//}
+		//
+		#endregion
+
+
+		/// <summary>
+		///A test for ToPlural
+		///</summary>
+		[TestMethod()]
+		public void ToPluralTest()
+		{
+			Assert.AreEqual("test".ToPlural2(), "tests"); // standard
+			Assert.AreEqual("goose".ToPlural2(), "geese"); // special nouns
+			Assert.AreEqual("box".ToPlural2(), "boxes"); // -ch, x, s to -es
+			Assert.AreEqual("boy".ToPlural2(), "boys"); // -y to -ies
+			Assert.AreEqual("box of ball".ToPlural2(), "box of balls"); // of
+			Assert.AreEqual("kiss".ToPlural2(), "kisses"); // -s to -es
+			Assert.AreEqual("phenomenon".ToPlural2(), "phenomena"); // nouns that maintain their Latin or Greek form in the plural
+			
+			Assert.AreEqual("potato".ToPlural2(), "potatoes"); // -o to -oes
+			Assert.AreEqual("memo".ToPlural2(), "memos"); // -o to -oes (exceptions)
+			Assert.AreEqual("stereo".ToPlural2(), "stereos"); // -o to -oes (exceptions)
+
+			Assert.AreEqual("knife".ToPlural2(), "knives"); // -f, fe to -es
+			// these two fail
+			// Assert.AreEqual("dwarf".ToPlural(), "dwarfs"); // -f, fe to -es (exceptions)
+			// Assert.AreEqual("roof".ToPlural(), "roofs"); // -f, fe to -es (exceptions)
+		}
+	}
+}
---
+++ PGK.Extensions4/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity.Design.PluralizationServices;
+using System.Globalization;
+
+/// <summary>
+/// 	Extension methods for the string data type
+/// </summary>
+public static partial class StringExtensions
+{
+	/// <summary>
+	/// Returns the plural form of the specified word.
+	/// </summary>
+	/// <param name="singular">The singular string value</param>
+	/// <param name="count">How many of the specified word there are. A count equal to 1 will not pluralize the specified word.</param>
+	/// <param name="cultureInfo">Provide a culture info to pluralize (default to en-US)</param>
+	/// <returns>A string that is the plural form of the input parameter.</returns>
+	public static string ToPlural2(this string singular, int count = 0, CultureInfo cultureInfo = null)
+	{
+		return count == 1 ? singular : PluralizationService.CreateService(cultureInfo ?? new CultureInfo("en-US")).Pluralize(singular);
+	}
+
+}
