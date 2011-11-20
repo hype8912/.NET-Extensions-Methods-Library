6193eacdd2b35fe2629673fa1d5acb042d96d3db
nagits_cp <SND\nagits_cp>
2011-11-20 13:48:43 +0000

    + EnumerableExtensions.ToDictionary(this IEnumerable<IGrouping<TKey, TValue>> groupings), returns Dictionary<TKey, List<TValue>> + ComboBoxExtensions.MeasureDropDownWidth(this ComboBox comboBox, ...) - automatic calculation of the required width of the drop-down list

--- PGK.Extensions/EnumerableExtensions.cs
+++ PGK.Extensions/EnumerableExtensions.cs
@@ -484,4 +484,16 @@ public static class EnumerableExtensions
     {
         return source.Select(selector).Sum();
     }
+
+    /// <summary>
+    /// Converts an enumeration of groupings into a Dictionary of those groupings.
+    /// </summary>
+    /// <typeparam name="TKey">Key type of the grouping and dictionary.</typeparam>
+    /// <typeparam name="TValue">Element type of the grouping and dictionary list.</typeparam>
+    /// <param name="groupings">The enumeration of groupings from a GroupBy() clause.</param>
+    /// <returns>A dictionary of groupings such that the key of the dictionary is TKey type and the value is List of TValue type.</returns>
+    public static Dictionary<TKey, List<TValue>> ToDictionary<TKey, TValue>(this IEnumerable<IGrouping<TKey, TValue>> groupings)
+    {
+        return groupings.ToDictionary(group => group.Key, group => group.ToList());
+    }
 }
